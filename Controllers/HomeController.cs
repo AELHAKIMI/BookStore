a31a0c7a66d5b88927bc30211ba3c54c24e04874
EL HAKIMI <ayo-voi@hotmail.com>
2021-06-02 18:27:34 +0100

    add views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,12 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello from HomeController ";
+            return View();
+        }
+        public ViewResult AboutUs(){
+            return View();
         }
     }
 }
